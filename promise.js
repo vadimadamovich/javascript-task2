44a214f8e0979758450df15887c67fe1e5df664c
Vadim Adamovich <vadim.adamovich@gmail.com>
2020-05-13 03:54:09 +0200

    v1.1

--- promise.js
+++ promise.js
@@ -1,13 +1,10 @@
-var fn1 = function() { return new Promise(function(resolve, reject) { console.log('fn1'); return resolve(1); }); };
-var fn2 = function() { return new Promise(function(resolve, reject) { setTimeout(function() { console.log('fn2'); return resolve(2); }, 2000); }); };
+var fn1 = () => { console.log('fn1'); return Promise.resolve(1) }
+var fn2 = () => { return new Promise(resolve => { console.log('fn2'); setTimeout(() => resolve(2), 3000) })}
 
-function promiseReduce(asyncFunctions, reduce, initialValue) {
-  let value = initialValue;
-  asyncFunctions.forEach(async function(func) {
-    await func().then(function(res) { value = reduce(1, value); value = value + res; }, null);
-  });
-  return Promise.resolve(value);
+async function promiseReduce(asyncFunctions, reduce, initialValue) {
+    let value = initialValue
+    for (let i in asyncFunctions) value = reduce(await asyncFunctions[i](), value)
+    return Promise.resolve(value)
 }
 
-promiseReduce([fn1, fn2], function(memo, value) { console.log('reduce'); return memo * value; }, 1).then(console.log);
-
+promiseReduce([fn1, fn2], (memo, value) => { console.log('reduce'); return memo * value }, 1).then(console.log)
